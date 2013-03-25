1a02f2d9b08f0734d2744ea4d4ca59484be295a5
Peter Berkenbosch <peter@pero-ict.nl>
2013-03-25 10:14:41 +0100

    created initial spree 1-3-stable store. * rails 3.2.13 * rspec-rails * spree 1-3-stable * spree_auth_devise 1-3-stable

---
+++ README.md
@@ -0,0 +1,3 @@
+ # MTGSpree
+
+1-3-stable Spree webshop used during the "From Scratch" series on [SpreeCasts](http://spreecasts.org)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtgspree::Application.config.secret_token = '3dfb57e3884597bca168af6a9699d1840161f3af3efb2dd01814248b05e1ceb497c8c7285c47d473a3762c3b5a7506a82211fbfda31ba351f5f7a8fea7270065'
